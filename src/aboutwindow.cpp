f6ee211c8a35044c704b7b283f0f1100ca34a7b1
Eugene Melnik <jeka7js@gmail.com>
2014-10-04 13:50:52 +0300

    Clicable address

--- src/aboutwindow.cpp
+++ src/aboutwindow.cpp
@@ -9,7 +9,7 @@ AboutWindow::AboutWindow( QWidget* parent ) : QDialog( parent )
     lAppVersion->setText( tr( "version %1" ).arg( Alexandra::appVersionFull ) );
     lAuthor->setText( tr( "<b>Author(s):</b> %1" ).arg( Alexandra::appAuthor ) );
     lLicense->setText( tr( "<b>License:</b> %1" ).arg( Alexandra::appLicense ) );
-    lAddress->setText( tr( "<b>Address:</b> %1" ).arg( Alexandra::appAddress ) );
+    lAddress->setText( tr( "<b>Address:</b> <a href=\"%1\">Github</a>" ).arg( Alexandra::appAddress ) );
 
     // random image from resources
     image->setPixmap( QPixmap( QString( ":/cats/%1" ).arg( rand() % 6 + 1 ) ) );
