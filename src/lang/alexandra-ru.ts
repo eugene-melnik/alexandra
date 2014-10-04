31987229e69c40421521b814fee03bbf7ff5819b
Eugene Melnik <jeka7js@gmail.com>
2014-10-04 16:36:21 +0300

    Russian translation updated

--- src/lang/alexandra-ru.ts
+++ src/lang/alexandra-ru.ts
@@ -1,22 +1,215 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru" sourcelanguage="en">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="../aboutwindow.ui" line="29"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.cpp" line="9"/>
+        <source>version %1</source>
+        <translation>версия %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.cpp" line="10"/>
+        <source>&lt;b&gt;Author(s):&lt;/b&gt; %1</source>
+        <translation>&lt;b&gt;Автор(ы):&lt;/b&gt; %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.cpp" line="11"/>
+        <source>&lt;b&gt;License:&lt;/b&gt; %1</source>
+        <translation>&lt;b&gt;Лицензия:&lt;/b&gt; %1</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.cpp" line="12"/>
+        <source>&lt;b&gt;Address:&lt;/b&gt; &lt;a href=&quot;%1&quot;&gt;Github&lt;/a&gt;</source>
+        <translation>&lt;b&gt;Адрес:&lt;/b&gt; &lt;a href=&quot;%1&quot;&gt;Github&lt;/a&gt;</translation>
+    </message>
+</context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="28"/>
+        <location filename="../mainwindow.ui" line="53"/>
+        <source>Type keyword for filtering films list</source>
+        <translation>Введите ключевое слово для фильтрации списка фильмов</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="68"/>
+        <source>&lt;type to search&gt;</source>
+        <translation>&lt;введите для поиска&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="211"/>
+        <source>Is viewed</source>
+        <translation>Просмотрен</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="233"/>
+        <source>Is added to favourites</source>
+        <translation>Добавлен в избранное</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="255"/>
+        <source>Show technical information</source>
+        <translation>Показать техническую информацию</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="282"/>
+        <source>Open film in external player</source>
+        <translation>Открыть фильм во внешнем проигрывателе</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="285"/>
+        <source>PLAY</source>
+        <translation>Воспроизвести</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="288"/>
+        <source>Alt+Return</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="324"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="45"/>
+        <location filename="../mainwindow.ui" line="330"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="337"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Правка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="345"/>
+        <source>Fil&amp;ms</source>
+        <translation>Ф&amp;ильмы</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="374"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>В&amp;ыход</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="48"/>
+        <location filename="../mainwindow.ui" line="377"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="386"/>
+        <source>&amp;About...</source>
+        <translation>&amp;О программе...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="397"/>
+        <source>Show toolbar</source>
+        <translation>Показать панель инструментов</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="400"/>
+        <source>Ctrl+T</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="409"/>
+        <source>Preferences...</source>
+        <translation>Настройки...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="412"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="421"/>
+        <source>&amp;Add film...</source>
+        <translation>&amp;Добавить фильм...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="424"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="433"/>
+        <source>Edit film...</source>
+        <translation>Изменить фильм...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="436"/>
+        <source>Ctrl+E</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="445"/>
+        <source>Remove film</source>
+        <translation>Удалить фильм</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="448"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="453"/>
+        <source>&amp;Random</source>
+        <translation>С&amp;лучайный</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="456"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="465"/>
+        <source>&amp;Search...</source>
+        <translation>По&amp;иск...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="468"/>
+        <source>Ctrl+F</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="473"/>
+        <source>About Qt...</source>
+        <translation>О Qt...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="14"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="17"/>
+        <source>Edit</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="20"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <source>Random</source>
+        <translation>Случайный</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="28"/>
+        <source>Search</source>
+        <translation>Поиск</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="33"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
     </message>
 </context>
 </TS>
