e5018a8396b61ac5749e55517a7f4af966ff1db2
Eugene Melnik <jeka7js@gmail.com>
2015-02-15 23:03:37 +0200

    Copyright updated

--- src/film.h
+++ src/film.h
@@ -3,7 +3,7 @@
  *  file: film.h                                                                                  *
  *                                                                                                *
  *  Alexandra Video Library                                                                       *
- *  Copyright (C) 2014 Eugene Melnik <jeka7js@gmail.com>                                          *
+ *  Copyright (C) 2014-2015 Eugene Melnik <jeka7js@gmail.com>                                     *
  *                                                                                                *
  *  Alexandra is free software; you can redistribute it and/or modify it under the terms of the   *
  *  GNU General Public License as published by the Free Software Foundation; either version 2 of  *
